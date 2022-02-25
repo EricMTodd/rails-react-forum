e5107914dcf8197ffdf0086a2d63b2d9fe2f1cdb
ericmtodd <eterictodd@gmail.com>
2022-02-24 19:13:53 -0700

    Basic user registration form implemented. API call implementation still required.

--- app/javascript/packs/components/registrations/Create.js
+++ app/javascript/packs/components/registrations/Create.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('handleSubmit')
+  }
+
   return(
     <div id='sign-up'>
       <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>
+          <strong>Name</strong>
+          <br />
+          <input type='text' name='name' value={name} autoComplete='off' onChange={e => setName(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label htmlFor='email'>
+          <strong>Email</strong>
+          <br />
+          <input type='email' name='email' value={email} autoComplete='off' onChange={e => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label htmlFor='password'>
+          <strong>Password</strong>
+          <br />
+          <input type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label htmlFor='passwordConfirmation'>
+          <strong>Password confirmation</strong>
+          <br />
+          <input type='password' name='passwordConfirmation' value={passwordConfirmation} onChange={e => setPasswordConfirmation(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <button type='submit'>Sign Up</button>
+      </form>
     </div>
   )
 }
