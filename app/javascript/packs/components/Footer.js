0abb40728b17764c3a96daa8ef720d40a3e59258
ericmtodd <eterictodd@gmail.com>
2022-02-26 13:10:25 -0700

    Updated footer.

--- app/javascript/packs/components/Footer.js
+++ app/javascript/packs/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return(
     <footer>
-      Footer
+      Made with ❤️ by <a href='https://ericmtodd.herokuapp.com' target='_blank'>Eric M. Todd</a>
     </footer>
   )
 }
