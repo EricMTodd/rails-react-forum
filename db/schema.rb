a7204e4fa4c79a1079c4885f162e4d7b58b4e0aa
ericmtodd <eterictodd@gmail.com>
2022-02-28 21:59:49 -0700

    Working on nested comments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_064113) do
+ActiveRecord::Schema.define(version: 2022_03_01_045744) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
