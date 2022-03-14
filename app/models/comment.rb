6cff6d8c0de33fb843a3d40def5cc002b0749495
ericmtodd <eterictodd@gmail.com>
2022-03-14 16:53:52 -0600

    Updated model validations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id
+  validates :body, presence: true
 end
